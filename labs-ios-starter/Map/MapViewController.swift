d3cdf894c6db88c8e15ce9f364e07d3344810983
CaptainnClayton <60490642+CaptainnClayton@users.noreply.github.com>
2021-01-27 20:28:32 -0800

    Implementation of MapKit (#2)
    * Changed properties in Profile model: id, email, and name to be optional as per Johnny Hicks
    
    * Updated BaseURL to reflect backend changes and changed OktaAuth as per Johnny Hicks.
    
    * Created Map Storyboard and MapViewController.Swift
    
    * Added a storyboard reference to the new Map Storyboard. Added Mapkit to the view controller.
    
    * Added reference to the MapKitView controller. Map is now an option in the tab bar and can be selected.
    
    * Imported MapKit and CoreLocation. Instantiated a userTrackingButton and a CLLocationManager. Started work on getting user location.
    
    * Conformed to the CLLocationManagerDelegate in order to update locations
    
    * Added in function to add the tracking button to the view.
    
    * Added ViewWillAppear to call function regarding location. Updated info.plist to ask for permission for user location.
    
    Co-authored-by: Clayton Watkins <watkinclayton17@yahoo.com>

---
+++ labs-ios-starter/Map/MapViewController.swift
@@ -0,0 +1,63 @@
+//
+//  MapViewController.swift
+//  labs-ios-starter
+//
+//  Created by Clayton Watkins on 1/27/21.
+//  Copyright © 2021 Spencer Curtis. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class MapViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var mapView: MKMapView!
+    
+
+    // MARK: - Properties
+    var userLocationButton: MKUserTrackingButton!
+    let manager = CLLocationManager()
+
+
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        layoutTrackingButton()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        getUserLocation()
+    }
+    
+    // MARK: - Helper Methods
+    private func getUserLocation(){
+        manager.delegate = self
+        manager.requestWhenInUseAuthorization()
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.startUpdatingLocation()
+    }
+    
+    private func layoutTrackingButton() {
+        userLocationButton = MKUserTrackingButton(mapView: mapView)
+        userLocationButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(userLocationButton)
+        
+        NSLayoutConstraint.activate([
+            userLocationButton.leadingAnchor.constraint(equalTo: mapView.leadingAnchor, constant: 20),
+            mapView.bottomAnchor.constraint(equalTo: userLocationButton.bottomAnchor, constant: 40)
+            
+        ])
+    }
+}
+
+// MARK: - Extension/Delegate Methods
+extension MapViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let _ = locations.first{
+            manager.stopUpdatingLocation()
+        }
+    }
+}
