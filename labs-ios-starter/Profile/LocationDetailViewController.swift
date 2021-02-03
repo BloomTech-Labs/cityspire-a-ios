a8bd6b24898d16489bb49313d8a28ddfef19b8c8
Clayton Watkins <watkinclayton17@yahoo.com>
2021-02-02 18:58:56 -0800

    Created MyProfileViewController and LocationDetailViewController

---
+++ labs-ios-starter/Profile/LocationDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LocationDetailViewController.swift
+//  labs-ios-starter
+//
+//  Created by Clayton Watkins on 2/2/21.
+//  Copyright © 2021 Spencer Curtis. All rights reserved.
+//
+
+import UIKit
+
+class LocationDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
