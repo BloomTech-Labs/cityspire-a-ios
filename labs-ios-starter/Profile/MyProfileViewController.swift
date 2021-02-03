ab5cb4bb7581a8be44e154d33bbbe63638c91aa4
Clayton Watkins <watkinclayton17@yahoo.com>
2021-02-02 19:03:17 -0800

    Stubbed out MyProfileViewController and LocationDetailViewController

--- labs-ios-starter/Profile/MyProfileViewController.swift
+++ labs-ios-starter/Profile/MyProfileViewController.swift
@@ -10,21 +10,31 @@ import UIKit
 
 class MyProfileViewController: UIViewController {
 
+    // MARK: - IBOutlets
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
-
-    /*
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
+    }
+}
+
+// MARK: - Extensions
+extension MyProfileViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
 }
