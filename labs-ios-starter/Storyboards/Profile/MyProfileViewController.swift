a5d9ecc2d6a98c724bb3684d58c476a5753384cf
Jessie <jessie.griffin@icloud.com>
2021-02-03 23:09:51 -0500

    Adds IBOutlets and layer adjustments
    Also pulls in user information to display avatar, name and email

--- labs-ios-starter/Storyboards/Profile/MyProfileViewController.swift
+++ labs-ios-starter/Storyboards/Profile/MyProfileViewController.swift
@@ -12,19 +12,47 @@ class MyProfileViewController: UIViewController {
 
     // MARK: - IBOutlets
     @IBOutlet weak var tableView: UITableView!
-    
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailtextField: UITextField!
+    @IBOutlet weak var favoritePlacesLabel: UILabel!
+
     // MARK: - Properties
-    
+    var profileController: ProfileController = ProfileController.shared
+
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        setTextFieldAttributes()
     }
     
     // MARK: - Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
 
+    }
+
+    // MARK: - Private
+    private func setTextFieldAttributes() {
+        nameTextField.layer.cornerRadius = 18
+        nameTextField.layer.shadowOpacity = 0.3
+        nameTextField.layer.shadowRadius = 2.0
+        nameTextField.layer.shadowColor = UIColor.darkGray.cgColor
+        nameTextField.layer.shadowOffset = CGSize(width: 8, height: 8)
+        nameTextField.text = profileController.authenticatedUserProfile?.name
+
+        emailtextField.layer.cornerRadius = 18
+        emailtextField.layer.shadowOpacity = 0.3
+        emailtextField.layer.shadowRadius = 2.0
+        emailtextField.layer.shadowColor = UIColor.darkGray.cgColor
+        emailtextField.layer.shadowOffset = CGSize(width: 8, height: 8)
+        emailtextField.text = profileController.authenticatedUserProfile?.email
+
+        favoritePlacesLabel.layer.cornerRadius = 18
+        favoritePlacesLabel.layer.backgroundColor = CGColor(red: 255, green: 255, blue: 255, alpha: 0.4)
+
+        avatarImageView.image = profileController.authenticatedUserProfile?.avatarImage
     }
 }
 
@@ -39,7 +67,10 @@ extension MyProfileViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "LocationCell") else { return UITableViewCell()}
         // Test Cell
+        // MARK: TODO
+        // Create swift file for table view cell
         cell.textLabel?.text = "Location"
+        cell.detailTextLabel?.text = "Number of places for rent?"
         return cell
     }
 }
