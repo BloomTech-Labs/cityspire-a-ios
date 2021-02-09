987a310d5319d58d7bf5e08104ba118866e5ea6f
Jessie <jessie.griffin@icloud.com>
2021-02-08 21:58:33 -0500

    Adjusts constraints on TableViewCell and adds swift file for TableViewCell

---
+++ labs-ios-starter/Storyboards/Profile/LocationTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  LocationTableViewCell.swift
+//  labs-ios-starter
+//
+//  Created by Jessie Ann Griffin on 2/8/21.
+//  Copyright © 2021 Spencer Curtis. All rights reserved.
+//
+
+import UIKit
+
+class LocationTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var locationImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
