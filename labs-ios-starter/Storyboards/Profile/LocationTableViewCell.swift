d661cd1cc57cc9411eaa3fe695521b6cbd1fae4c
Jessie <threedaystoryllc@icloud.com>
2021-02-08 22:59:04 -0500

    Adds UI elements and Files for the user's My Profile View (#10)
    * Adds UI elements to User Profile VC in Storyboard
    
    Adds avatar image view, name and email text fields, title label for the table, and an image view for walkability score in item list.
    
    * Adds IBOutlets and layer adjustments
    
    Also pulls in user information to display avatar, name and email
    
    * TODO: Table View cell not displaying correctly
    
    * Updates some constraints on the table view
    
    * Adjusts constraints on TableViewCell and adds swift file for TableViewCell
    
    * Adjusts TableView setting so that the Cell height is correct
    
    Co-authored-by: Jessie <jessie.griffin@icloud.com>

---
+++ labs-ios-starter/Storyboards/Profile/LocationTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  LocationTableViewCell.swift
+//  labs-ios-starter
+//
+//  Created by Jessie Ann Griffin on 2/8/21.
+//  Copyright © 2021 Spencer Curtis. All rights reserved.
+//
+
+import UIKit
+
+class LocationTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var locationImageView: UIImageView!
+    
+
+}
