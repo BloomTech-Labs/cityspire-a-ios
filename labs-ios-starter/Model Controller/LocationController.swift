a6d938169403315d90ca03af2ad26667a6288616
Clayton Watkins <watkinclayton17@yahoo.com>
2021-02-16 19:14:27 -0800

    Created Location Controller File

---
+++ labs-ios-starter/Model Controller/LocationController.swift
@@ -0,0 +1,9 @@
+//
+//  LocationController.swift
+//  labs-ios-starter
+//
+//  Created by Clayton Watkins on 2/16/21.
+//  Copyright © 2021 Spencer Curtis. All rights reserved.
+//
+
+import Foundation
