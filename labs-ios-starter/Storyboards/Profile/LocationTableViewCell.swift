64f98baa973581ebebdd0f0e9b6fe6b464e18354
Jessie <jessie.griffin@icloud.com>
2021-02-17 22:21:12 -0500

    Updated TableViewCell to include WalkScore, CrimeRate, and RentalRate as scaling images.

--- labs-ios-starter/Storyboards/Profile/LocationTableViewCell.swift
+++ labs-ios-starter/Storyboards/Profile/LocationTableViewCell.swift
@@ -12,7 +12,13 @@ class LocationTableViewCell: UITableViewCell {
     // MARK: - IBOutlet
     @IBOutlet weak var locationNameLabel: UILabel!
     
+    @IBOutlet weak var walkScoreDisplay: UILabel!
+    @IBOutlet weak var crimeRateDisplay: UILabel!
+    @IBOutlet weak var rentalRateDisplay: UILabel!
+
     // MARK: - Properties
+    let locationController = LocationController.shared
+
     var location: ReturnedLocation? {
         didSet{
             updateViews()
@@ -23,5 +29,46 @@ class LocationTableViewCell: UITableViewCell {
     private func updateViews() {
         guard let location = location else { return }
         locationNameLabel.text = location.name
+        locationController.getCityDetails(name: location.name) { (returnedLocation) in
+            do {
+                let result = try returnedLocation.get()
+                DispatchQueue.main.async {
+                    switch result.walkScore {
+                    case 0...35:
+                        self.walkScoreDisplay.text = "🚶‍♂️"
+                    case 36...70:
+                        self.walkScoreDisplay.text = "🚶🏽‍♀️🚶🏼"
+                    case 71...100:
+                        self.walkScoreDisplay.text = "🚶🏽‍♀️🚶🏼🚶‍♂️"
+                    default:
+                        self.walkScoreDisplay.text = "\(result.walkScore)"
+                    }
+
+                    switch result.crimeRate {
+                    case 0...35:
+                        self.crimeRateDisplay.text = "🦹🏼‍♂️"
+                    case 36...70:
+                        self.crimeRateDisplay.text = "🦹🏼‍♂️🦹🏼‍♂️"
+                    case 71...100:
+                        self.crimeRateDisplay.text = "🦹🏼‍♂️🦹🏼‍♂️🦹🏼‍♂️"
+                    default:
+                        self.crimeRateDisplay.text = "\(result.crimeRate)"
+                    }
+
+                    switch result.rentalRate {
+                    case 0...1000:
+                        self.rentalRateDisplay.text = "$"
+                    case 1001...1500:
+                        self.rentalRateDisplay.text = "$$"
+                    case 1501...10000:
+                        self.rentalRateDisplay.text = "$$$"
+                    default:
+                        self.rentalRateDisplay.text = "\(result.rentalRate)"
+                    }
+                }
+            } catch {
+                print("Error getting location data: \(error)")
+            }
+        }
     }
 }
