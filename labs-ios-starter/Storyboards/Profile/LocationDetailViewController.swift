dd354c69bbf261b6d7b36865feebe4b16dca9e35
Jessie <jessie.griffin@icloud.com>
2021-02-17 23:56:04 -0500

    Adds a map view and one working lottie gif (so far)
    Removes the locationNameLabel and sets the title of the view to the name of the location. Added a mapview but I still need to connect the location name so that at least a static zoomed view of the city shows in the map view.

--- labs-ios-starter/Storyboards/Profile/LocationDetailViewController.swift
+++ labs-ios-starter/Storyboards/Profile/LocationDetailViewController.swift
@@ -7,16 +7,20 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
+import Lottie
 
 class LocationDetailViewController: UIViewController {
     
     // MARK: - IBOutlets
-    @IBOutlet weak var locationNameLabel: UILabel!
     @IBOutlet weak var populationLabel: UILabel!
     @IBOutlet weak var crimeLabel: UILabel!
     @IBOutlet weak var rentalLabel: UILabel!
     @IBOutlet weak var walkScoreLabel: UILabel!
-    
+    @IBOutlet weak var detailMapView: MKMapView!
+    @IBOutlet weak var walkingAnimationView: UIView!
+
     // MARK: - Properties
     let locationController = LocationController.shared
     var locationName: String? {
@@ -24,17 +28,28 @@ class LocationDetailViewController: UIViewController {
             updateViews()
         }
     }
-    
+    let walkingAnimation = Animation.named("39992-walking")
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let animationView = AnimationView(animation: walkingAnimation)
+        animationView.frame = CGRect(x: 0, y: 0, width: 60, height: 60)
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = .loop
+
+        walkingAnimationView.addSubview(animationView)
+        animationView.play()
+    }
     // MARK: - Private Methods
     private func updateViews() {
         guard let locationName = locationName else { return }
+        title = locationName
         locationController.getCityDetails(name: locationName) { (returnedLocation) in
             do {
                 let result = try returnedLocation.get()
                 DispatchQueue.main.async {
-                    self.locationNameLabel.text = self.locationName
-                    self.populationLabel.text = "\(result.population)"
-                    self.crimeLabel.text = "\(result.crimeRate)"
+                    self.populationLabel.text = "\(result.population.formattedWithSeparator) humans"
+                    self.crimeLabel.text = "\(result.crimeRate)%"
                     self.rentalLabel.text = "\(result.rentalRate)"
                     self.walkScoreLabel.text = "\(result.walkScore)"
                 }
@@ -45,4 +60,16 @@ class LocationDetailViewController: UIViewController {
     }
 }
 
+// MARK: - Extensions
+extension Formatter {
+    static let withSeparator: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.groupingSeparator = ","
+        return formatter
+    }()
+}
 
+extension Numeric {
+    var formattedWithSeparator: String { Formatter.withSeparator.string(for: self) ?? "" }
+}
