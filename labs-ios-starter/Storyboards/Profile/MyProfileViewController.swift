a67422a70065cd72e01e2f4c88e2b463f0d86203
Clayton Watkins <watkinclayton17@yahoo.com>
2021-02-22 20:04:20 -0800

    Added colors to the tableView, added animations to the cells as they are loaded into the app

--- labs-ios-starter/Storyboards/Profile/MyProfileViewController.swift
+++ labs-ios-starter/Storyboards/Profile/MyProfileViewController.swift
@@ -28,6 +28,7 @@ class MyProfileViewController: UIViewController {
         tableView.delegate = self
         tableView.dataSource = self
         setTextFieldAttributes()
+        tableView.backgroundColor = ColorsHelper.sonicSilver
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -38,6 +39,7 @@ class MyProfileViewController: UIViewController {
                 self.mySavedLocations = returnedLocations
                 DispatchQueue.main.async {
                     self.tableView.reloadData()
+                    self.animateTable()
                 }
             } catch {
                 print("Error getting back saved location data: \(error)")
@@ -75,7 +77,28 @@ class MyProfileViewController: UIViewController {
 
         avatarImageView.image = profileController.authenticatedUserProfile?.avatarImage
     }
+    
+    private func animateTable() {
+        let cells = tableView.visibleCells
+        
+//        let tableViewHeight = tableView.bounds.size.height
+        let tableViewWidth = tableView.bounds.size.width
+        
+        for cell in cells {
+            cell.transform = CGAffineTransform(translationX: -tableViewWidth, y: 0)
+        }
+        var delayCounter = 0
+        
+        for cell in cells {
+            UIView.animate(withDuration: 1.75, delay: Double(delayCounter) * 0.05, usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: .curveEaseInOut, animations: {
+                cell.transform = CGAffineTransform.identity
+                }, completion: nil)
+            delayCounter += 1
+        }
+    }
 }
+
+
 
 // MARK: - Extensions
 extension MyProfileViewController: UITableViewDelegate, UITableViewDataSource {
@@ -87,7 +110,7 @@ extension MyProfileViewController: UITableViewDelegate, UITableViewDataSource {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "LocationCell") as? LocationTableViewCell else { return UITableViewCell()}
         cell.location = mySavedLocations[indexPath.row]
         cell.contentView.layer.cornerRadius = 15
-        cell.contentView.layer.backgroundColor = ColorsHelper.harvestGold.cgColor
+        cell.contentView.layer.backgroundColor = ColorsHelper.lightFrenchBeige.cgColor
         return cell
     }
     
